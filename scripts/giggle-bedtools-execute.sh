6c0e3c98ef484d677adf2277ba852b0d25ef52d8
Christopher <76705012+ChristopherEbukaOjukwu@users.noreply.github.com>
2023-11-22 23:49:36 -0700

    Update giggle-bedtools-execute.sh

--- scripts/giggle-bedtools-execute.sh
+++ scripts/giggle-bedtools-execute.sh
@@ -4,7 +4,7 @@ while IFS= read -r line; do
         e=$( echo "$line" | cut -f3 )
         gene=$( echo "$line" | cut -f4 )
 
-        hits=$( bedtools intersect -a sorted_hg19.protein_coding.bed.gz -b <(giggle search -i input.d/ -v -r $chr:$s-$e | cut -f 5-7) | cut -f4 | paste -sd, - )
+        hits=$( bedtools intersect -a sorted_hg19.protein_coding.bed.gz -b <(giggle search -i alt_sort_b/ -v -r $chr:$s-$e | cut -f 5-7) | cut -f4 | paste -sd, - )
         echo -e "$gene\t$hits"
 
 

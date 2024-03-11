0c971507fd482d8df6f2fa6adbfa81c7842077cb
Christopher <76705012+ChristopherEbukaOjukwu@users.noreply.github.com>
2024-03-11 11:50:14 -0600

    Update bed-gig-build.sh

--- scripts/bed-gig-build.sh
+++ scripts/bed-gig-build.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install -y \
     gcc make autoconf zlib1g-dev libbz2-dev libcurl4-openssl-dev libssl-dev ruby \
-    g++ python liblzma-dev pysam\
+    g++ python liblzma-dev\
    
 
 
